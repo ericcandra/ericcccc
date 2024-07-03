88d734347f5693f49efeaa026854806fb7f654b4
ericcandra <160827840+ericcandra@users.noreply.github.com>
2024-07-03 20:38:54 +0700

    loggg

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -147,9 +147,11 @@
                 <ul class="list-unstyled">
                   <li><a href="{{url ('dashboard')}}">Dashboard</a></li>
                   <li><a href="{{url ('home')}}">Home</a></li>
+                  <li><a href="{{url ('home')}}">Buku</a></li>
                   <li><a href="{{url ('anggota')}}">Anggota</a></li>
                   <li><a href="{{url ('petugas')}}">Petugas</a></li>
                   <li><a href="{{url ('pinjam')}}" >Peminjaman</a></li>
+                  <li><a href="{{url ('logout')}}">Logout</a></li>
                 </ul>
               </div>
               
