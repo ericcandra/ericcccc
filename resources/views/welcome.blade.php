fcd34fbebbd5eeb69b1ca65b2b3f7794c3ec92b1
ericcandra <160827840+ericcandra@users.noreply.github.com>
2024-07-06 11:17:33 +0700

    nap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,8 @@
                                 class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                             >
                                 <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
-                                    <img
+                                    <img src="{{url('images')}}/gambar1p.jpg" width="20%" height="40px">
+                                    {{-- <img
                                         src="gambar1p.jpg"
                                         alt="gambar1p.jpg"
                                         class="aspect-video h-full w-full flex-1 rounded-[10px] object-top object-cover drop-shadow-[0px_4px_34px_rgba(0,0,0,0.06)] dark:hidden"
@@ -73,7 +74,7 @@
                                             document.getElementById('docs-card-content').classList.add('!flex-row');
                                             document.getElementById('background').classList.add('!hidden');
                                         "
-                                    />
+                                    /> --}}
                                     <img
                                         src="https://laravel.com/assets/img/welcome/docs-dark.svg"
                                         alt="Laravel documentation screenshot"
